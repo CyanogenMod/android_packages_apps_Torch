b0d18b3cfedf911e365c98e9e3b7ae1d2976819d
Michael Webster <miketwebster@gmail.com>
2010-12-04 18:21:27 -0500

    Obey torch "brightness" setting in notification and power bar widgets.
    Change-Id: I2f596c7f1747a711c6a4702c29ab620a20293368

--- src/net/cactii/flash2/TorchSwitch.java
+++ src/net/cactii/flash2/TorchSwitch.java
@@ -9,21 +9,28 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 
 public class TorchSwitch extends BroadcastReceiver {
 
     public static final String TOGGLE_FLASHLIGHT = "net.cactii.flash2.TOGGLE_FLASHLIGHT";
     public static final String TORCH_STATE_CHANGED = "net.cactii.flash2.TORCH_STATE_CHANGED";
+
+    private SharedPreferences mPrefs;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (intent.getAction().equals(TOGGLE_FLASHLIGHT)) {
             Intent i = new Intent(context, TorchService.class);
             if (this.TorchServiceRunning(context)) {
                 context.stopService(i);
                 Settings.System.putInt(context.getContentResolver(), Settings.System.TORCH_STATE, 0);
             } else {
+                i.putExtra("bright", mPrefs.getBoolean("bright", false));
                 context.startService(i);
                 Settings.System.putInt(context.getContentResolver(), Settings.System.TORCH_STATE, 1);
             }
