eb4d9d143dc3898e51ae062aa38a82769aff83e2
Marius Volkhart <marius@volkhart.com>
2012-05-09 01:26:01 -0500

    Torch: modified widget setup UI
    -Replaced "add widget" preference with a menu button as selector
    -Minor code cleanup to match android style guidelines
    
    Change-Id: I5f378cc2eb1f0c53d94ef5a6293883d000ccb007

--- src/net/cactii/flash2/WidgetOptionsActivity.java
+++ src/net/cactii/flash2/WidgetOptionsActivity.java
@@ -10,10 +10,11 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.RemoteViews;
 
 public class WidgetOptionsActivity extends PreferenceActivity implements
@@ -25,12 +26,13 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
 
     private SharedPreferences mPreferences;
 
+    @SuppressWarnings("deprecation")
+    //No need to go to fragments right now
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.optionsview);
         this.mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null) {
             mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
@@ -42,62 +44,56 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
         CheckBoxPreference mStrobePref = (CheckBoxPreference) findPreference("widget_strobe");
         mStrobePref.setChecked(false);
 
+        //TODO: Loading from res doesn't work. Also Hz range should be same as main app.
         mStrobeFrequency = (SeekBarPreference) findPreference("widget_strobe_freq");
         mStrobeFrequency.setEnabled(false);
 
+        //keeps 'Strobe frequency' option available
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-        Preference mSave = (Preference) findPreference("saveSettings");
-        mSave.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                Editor editor = mPreferences.edit();
-
-                editor.putBoolean("widget_strobe_" + mAppWidgetId,
-                        mPreferences.getBoolean("widget_strobe", false));
-                //had to do +1 to fix division by zero crash, only temporary fix:
-                editor.putInt("widget_strobe_freq_" + mAppWidgetId,
-                        666 / (1 + mPreferences.getInt("widget_strobe_freq", 5)));
-                editor.putBoolean("widget_bright_" + mAppWidgetId,
-                        mPreferences.getBoolean("widget_bright", false));
-                editor.commit();
-
-                //Initialize widget view for first update
-                Context context = getApplicationContext();
-                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-                Intent launchIntent = new Intent();
-                launchIntent.setClass(context, TorchWidgetProvider.class);
-                launchIntent.addCategory(Intent.CATEGORY_ALTERNATIVE);
-                launchIntent.setData(Uri.parse("custom:" + mAppWidgetId + "/0"));
-                PendingIntent pi = PendingIntent.getBroadcast(context, 0 /*
-                                                                          * no
-                                                                          * requestCode
-                                                                          */, launchIntent, 0 /*
-                                                                                               * no
-                                                                                               * flags
-                                                                                               */);
-                views.setOnClickPendingIntent(R.id.btn, pi);
-                if (mPreferences.getBoolean("widget_strobe_" + mAppWidgetId, false))
-                    views.setTextViewText(R.id.ind, context.getString(R.string.label_strobe));
-                else if (mPreferences.getBoolean("widget_bright_" + mAppWidgetId, false))
-                    views.setTextViewText(R.id.ind, context.getString(R.string.label_high));
-                else
-                    views.setTextViewText(R.id.ind, context.getString(R.string.widget_label));
-                final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-                appWidgetManager.updateAppWidget(mAppWidgetId, views);
-
-                Intent resultValue = new Intent();
-                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-                setResult(RESULT_OK, resultValue);
-                finish();
-                return false;
-            }
-
-        });
     }
 
-    public void onPause() {
-        super.onPause();
+    public void addWidget() {
+        Editor editor = mPreferences.edit();
+        editor.putBoolean("widget_strobe_" + mAppWidgetId, mPreferences.getBoolean("widget_strobe", false));
+
+        //TODO: Fix temporary patch
+        //had to do +1 to fix division by zero crash, only temporary fix:
+        editor.putInt("widget_strobe_freq_" + mAppWidgetId, 666 / (1 + mPreferences.getInt("widget_strobe_freq", 5)));
+        editor.putBoolean("widget_bright_" + mAppWidgetId, mPreferences.getBoolean("widget_bright", false));
+        editor.commit();
+
+        //Initialize widget view for first update
+        Context context = getApplicationContext();
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+        Intent launchIntent = new Intent();
+        launchIntent.setClass(context, TorchWidgetProvider.class);
+        launchIntent.addCategory(Intent.CATEGORY_ALTERNATIVE);
+        launchIntent.setData(Uri.parse("custom:" + mAppWidgetId + "/0"));
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0 /*
+                                                                  * no
+                                                                  * requestCode
+                                                                  */, launchIntent, 0 /*
+                                                                                       * no
+                                                                                       * flags
+                                                                                       */);
+        views.setOnClickPendingIntent(R.id.btn, pi);
+        if (mPreferences.getBoolean("widget_strobe_" + mAppWidgetId, false)) {
+            views.setTextViewText(R.id.ind, context.getString(R.string.label_strobe));
+        } else if (mPreferences.getBoolean("widget_bright_" + mAppWidgetId, false)) {
+            views.setTextViewText(R.id.ind, context.getString(R.string.label_high));
+        } else {
+            views.setTextViewText(R.id.ind, context.getString(R.string.widget_label));
+        }
+
+        final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        appWidgetManager.updateAppWidget(mAppWidgetId, views);
+
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+
+        //close the activity
+        finish();
     }
 
     @Override
@@ -106,5 +102,23 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
             this.mStrobeFrequency.setEnabled(sharedPreferences.getBoolean("widget_strobe", false));
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.widget, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saveSetting : //Changes are accepted
+                addWidget();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
