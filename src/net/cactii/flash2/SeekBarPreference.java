8acc1dce735fd50c4d41863329036c0b3e5768ed
Marius Volkhart <marius@volkhart.com>
2012-07-15 04:02:56 -0500

    Torch: Code and res cleanup
    -Added a lint config file for improved logging and problem detection
    -Performed lint cleanup
    
    Change-Id: Ic4e09d3ff7150b7fac5c7c23a4e0f3e0391d641d

--- src/net/cactii/flash2/SeekBarPreference.java
+++ src/net/cactii/flash2/SeekBarPreference.java
@@ -92,7 +92,7 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
         mValueText.setText(mSuffix == null ? t : t.concat(mSuffix));
         if (shouldPersist())
             persistInt(value);
-        callChangeListener(new Integer(value));
+        callChangeListener(Integer.valueOf(value));
     }
 
     public void onStartTrackingTouch(SeekBar seek) {
