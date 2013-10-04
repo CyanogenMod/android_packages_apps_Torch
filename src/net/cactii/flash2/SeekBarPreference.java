6442882f24a995587e8237ea50627281146a8a41
Marius Volkhart <marius@volkhart.com>
2013-10-04 00:03:19 -0500

    UI overhaul & code cleanup
    - Use Switch views for high brightness and strobe. This gives
      bigger touch targets and a cleaner implementation.
    - Move the menu into a dedicated resource instead of creating a
      submenu in code.
    - Hide frequency picker when not using strobe.
    - Fix logic for saving all preferences.
    - Remove old strings that are not and were not needed.
    - Other misc. cleanup
    
    Change-Id: I86a2a9415b394c7a0b0bffd1d166ee118fe4d65f

--- src/net/cactii/flash2/SeekBarPreference.java
+++ src/net/cactii/flash2/SeekBarPreference.java
@@ -16,55 +16,50 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class SeekBarPreference extends DialogPreference implements SeekBar.OnSeekBarChangeListener {
-
     private SeekBar mSeekBar;
-
     private TextView mSplashText, mValueText;
-
-    private Context mContext;
-
     private String mDialogMessage, mSuffix;
-
     private int mDefault, mMax, mValue = 0;
 
     public SeekBarPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext = context;
 
         mDialogMessage = context.getString(R.string.setting_frequency_dialog);
         mSuffix = context.getString(R.string.setting_frequency_hz);
         //has min value of 1hz but displays 0hz
         mDefault = 4;
         mMax = 24;
-
     }
 
     @Override
     protected View onCreateDialogView() {
+        final Context context = getContext();
         LinearLayout.LayoutParams params;
-        LinearLayout layout = new LinearLayout(mContext);
+        LinearLayout layout = new LinearLayout(context);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setPadding(6, 6, 6, 6);
 
-        mSplashText = new TextView(mContext);
-        if (mDialogMessage != null)
+        mSplashText = new TextView(context);
+        if (mDialogMessage != null) {
             mSplashText.setText(mDialogMessage);
+        }
         layout.addView(mSplashText);
 
-        mValueText = new TextView(mContext);
+        mValueText = new TextView(context);
         mValueText.setGravity(Gravity.CENTER_HORIZONTAL);
         mValueText.setTextSize(32);
         params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         layout.addView(mValueText, params);
 
-        mSeekBar = new SeekBar(mContext);
+        mSeekBar = new SeekBar(context);
         mSeekBar.setOnSeekBarChangeListener(this);
         layout.addView(mSeekBar, new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
-        if (shouldPersist())
+        if (shouldPersist()) {
             mValue = getPersistedInt(mDefault);
+        }
 
         mSeekBar.setMax(mMax);
         mSeekBar.setProgress(mValue);
@@ -81,23 +76,28 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
     @Override
     protected void onSetInitialValue(boolean restore, Object defaultValue) {
         super.onSetInitialValue(restore, defaultValue);
-        if (restore)
+        if (restore) {
             mValue = shouldPersist() ? getPersistedInt(mDefault) : 0;
-        else
+        } else {
             mValue = (Integer) defaultValue;
+        }
     }
 
+    @Override
     public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
-        String t = String.valueOf(value+1);
+        String t = String.valueOf(value + 1);
         mValueText.setText(mSuffix == null ? t : t.concat(mSuffix));
-        if (shouldPersist())
+        if (shouldPersist()) {
             persistInt(value);
+        }
         callChangeListener(Integer.valueOf(value));
     }
 
+    @Override
     public void onStartTrackingTouch(SeekBar seek) {
     }
 
+    @Override
     public void onStopTrackingTouch(SeekBar seek) {
     }
 
@@ -111,8 +111,9 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
 
     public void setProgress(int progress) {
         mValue = progress;
-        if (mSeekBar != null)
+        if (mSeekBar != null) {
             mSeekBar.setProgress(progress);
+        }
     }
 
     public int getProgress() {
--- src/net/cactii/flash2/WidgetOptionsActivity.java
+++ src/net/cactii/flash2/WidgetOptionsActivity.java
@@ -21,28 +21,28 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
         OnSharedPreferenceChangeListener {
 
     private int mAppWidgetId;
-
     private SeekBarPreference mStrobeFrequency;
-
     private SharedPreferences mPreferences;
 
     @SuppressWarnings("deprecation")
     //No need to go to fragments right now
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.layout.optionsview);
-        this.mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
         }
 
-        CheckBoxPreference mBrightPref = (CheckBoxPreference) findPreference("widget_bright");
-        mBrightPref.setChecked(false);
+        CheckBoxPreference brightPref = (CheckBoxPreference) findPreference("widget_bright");
+        brightPref.setChecked(false);
 
-        CheckBoxPreference mStrobePref = (CheckBoxPreference) findPreference("widget_strobe");
-        mStrobePref.setChecked(false);
+        CheckBoxPreference strobePref = (CheckBoxPreference) findPreference("widget_strobe");
+        strobePref.setChecked(false);
 
         mStrobeFrequency = (SeekBarPreference) findPreference("widget_strobe_freq");
         mStrobeFrequency.setEnabled(false);
@@ -53,12 +53,15 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
 
     public void addWidget() {
         Editor editor = mPreferences.edit();
-        editor.putBoolean("widget_strobe_" + mAppWidgetId, mPreferences.getBoolean("widget_strobe", false));
 
+        editor.putBoolean("widget_strobe_" + mAppWidgetId,
+                mPreferences.getBoolean("widget_strobe", false));
         //TODO: Fix temporary patch
         //had to do +1 to fix division by zero crash, only temporary fix:
-        editor.putInt("widget_strobe_freq_" + mAppWidgetId, 666 / (1 + mPreferences.getInt("widget_strobe_freq", 5)));
-        editor.putBoolean("widget_bright_" + mAppWidgetId, mPreferences.getBoolean("widget_bright", false));
+        editor.putInt("widget_strobe_freq_" + mAppWidgetId,
+                666 / (1 + mPreferences.getInt("widget_strobe_freq", 5)));
+        editor.putBoolean("widget_bright_" + mAppWidgetId,
+                mPreferences.getBoolean("widget_bright", false));
         editor.commit();
 
         //Initialize widget view for first update
@@ -68,14 +71,11 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
         launchIntent.setClass(context, TorchWidgetProvider.class);
         launchIntent.addCategory(Intent.CATEGORY_ALTERNATIVE);
         launchIntent.setData(Uri.parse("custom:" + mAppWidgetId + "/0"));
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0 /*
-                                                                  * no
-                                                                  * requestCode
-                                                                  */, launchIntent, 0 /*
-                                                                                       * no
-                                                                                       * flags
-                                                                                       */);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context,
+                0 /* no requestCode */, launchIntent, 0 /* no flags */);
         views.setOnClickPendingIntent(R.id.btn, pi);
+
         if (mPreferences.getBoolean("widget_strobe_" + mAppWidgetId, false)) {
             views.setTextViewText(R.id.ind_text, context.getString(R.string.label_strobe));
         } else if (mPreferences.getBoolean("widget_bright_" + mAppWidgetId, false)) {
@@ -96,9 +96,8 @@ public class WidgetOptionsActivity extends PreferenceActivity implements
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals("widget_strobe")) {
-            this.mStrobeFrequency.setEnabled(sharedPreferences.getBoolean("widget_strobe", false));
+            mStrobeFrequency.setEnabled(sharedPreferences.getBoolean("widget_strobe", false));
         }
-
     }
 
     @Override
