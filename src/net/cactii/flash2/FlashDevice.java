7e678c7b070e5be617f0bc612dbed4ffe042c9f4
Flamefire <alex@grundis.de>
2014-01-16 00:11:22 +0100

    Fix crash of torch app
    A hard to track bug occurs when using the torch:
    The local variable surfaceTexture gets collected by GC causing it
    to free the underlying buffer.
    The next access to this buffer crashes with an SIGSEGV after an
    assertion failure (BufferQueue already abandoned)
    
    NOTE: The native camera object may possibly hold that reference so
    it does not get GC'ed
    
    Change-Id: Ib39d55ffb379ed16a0b94915c1b727e8ddc7f180

--- src/net/cactii/flash2/FlashDevice.java
+++ src/net/cactii/flash2/FlashDevice.java
@@ -65,6 +65,7 @@ public class FlashDevice {
     private int mFlashMode = OFF;
 
     private Camera mCamera = null;
+    private SurfaceTexture mSurfaceTexture = null;
 
     private FlashDevice(Context context) {
         mValueOff = context.getResources().getInteger(R.integer.valueOff);
@@ -129,34 +130,18 @@ public class FlashDevice {
                         mCamera.release();
                         mCamera = null;
                         mSurfaceCreated = false;
+                        if(mSurfaceTexture != null) {
+                            mSurfaceTexture.release();
+                            mSurfaceTexture = null;
+                        }
                     }
                     if (mWakeLock.isHeld())
                         mWakeLock.release();
                 } else {
                     if (!mSurfaceCreated) {
-                        int[] textures = new int[1];
-                        // generate one texture pointer and bind it as an
-                        // external texture.
-                        GLES20.glGenTextures(1, textures, 0);
-                        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                                textures[0]);
-                        // No mip-mapping with camera source.
-                        GLES20.glTexParameterf(
-                                GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                                GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
-                        GLES20.glTexParameterf(
-                                GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                                GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-                        // Clamp to edge is only option.
-                        GLES20.glTexParameteri(
-                                GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                                GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
-                        GLES20.glTexParameteri(
-                                GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                                GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
-
-                        SurfaceTexture surfaceTexture = new SurfaceTexture(textures[0]);
-                        mCamera.setPreviewTexture(surfaceTexture);
+                        // Create a dummy texture, otherwhise setPreview won't work on some devices
+                        mSurfaceTexture = new SurfaceTexture(0);
+                        mCamera.setPreviewTexture(mSurfaceTexture);
                         mSurfaceCreated = true;
                         mCamera.startPreview();
                     }
