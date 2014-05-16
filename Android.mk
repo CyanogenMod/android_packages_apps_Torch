682fe903ffdce1c1b02871547f32e4be85c696e9
Roman Birg <roman@cyngn.com>
2014-05-16 10:22:39 -0700

    Torch: only include src files from src dir
    Currently if any other folders are present with java
    files(especially generated ones), the build system picks them up.
    
    Change-Id: I15d9c66844ab7a0c300f51a02b878d1dda4316fe
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- Android.mk
+++ Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := Torch
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
